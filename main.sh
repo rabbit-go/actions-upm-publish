dbbdac90fb437fb961cda76375965306846a76aa
rabbitbooster <yusuke.kikuchi@gmail.com>
2020-04-13 22:25:32 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,10 +10,10 @@ ${INPUT_RELEASE_SUMMARY}
 
 $(echo "${INPUT_RELEASE_BODY}" | sed 's/^#/\#\#/')
 EOS
-cat Assets/package.json | jq -Mr '. | .version = "'"${INPUT_RELEASE_VERSION##v}"'"' > /tmp/package.json
-mv /tmp/package.json Assets/package.json
+cat package.json | jq -Mr '. | .version = "'"${INPUT_RELEASE_VERSION##v}"'"' > /tmp/package.json
+mv /tmp/package.json package.json
 echo $(cat .npmrc | grep '^registry=' | sed 's/^registry=https://')'/:_authToken="'${INPUT_NPM_AUTH_TOKEN}'"' >> ~/.npmrc
-npm publish Assets
+npm publish 
 
 git config --global user.email "github-actions@example.com"
 git config --global user.name "GitHub Actions"
